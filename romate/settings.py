6a3ffee273c0edc2827267ee1e745611e06366cd
DONGhunee <37871553+DONGhunee@users.noreply.github.com>
2019-04-02 03:23:06 +0900

    de

--- romate/settings.py
+++ romate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '0u+qlu@(2l6tzj57d)9np)vk5@_^xk
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
